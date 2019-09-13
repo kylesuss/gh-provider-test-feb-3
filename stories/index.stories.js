d324f5152b71ee96d055b47a304e58c14f477b4f
Kyle Suss <susskyle@gmail.com>
2019-09-13 15:53:18 -0600

    Change it

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,12 +1,12 @@
-import { document, console } from 'global';
-import { storiesOf } from '@storybook/html';
+import { document, console } from "global";
+import { storiesOf } from "@storybook/html";
 
-storiesOf('Demo', module)
-  .add('heading', () => '<h1>Hello World</h1>')
-  .add('button', () => {
-    const button = document.createElement('button');
-    button.type = 'button';
-    button.innerText = 'Hello Buttonz';
-    button.addEventListener('click', e => console.log(e));
+storiesOf("Demo", module)
+  .add("heading", () => "<h1>Hello Worldz</h1>")
+  .add("button", () => {
+    const button = document.createElement("button");
+    button.type = "button";
+    button.innerText = "Hello Buttonz";
+    button.addEventListener("click", e => console.log(e));
     return button;
   });
