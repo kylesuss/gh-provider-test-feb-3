b2cd9e1f978dfee5e3e3c87e9a2116c8f452b515
Kyle Suss <susskyle@gmail.com>
2019-09-13 15:59:09 -0600

    Blue it

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -2,10 +2,11 @@ import { document, console } from "global";
 import { storiesOf } from "@storybook/html";
 
 storiesOf("Demo", module)
-  .add("heading", () => "<h1>Hello Worldz</h1>")
+  .add("heading", () => "<h1>Hello World</h1>")
   .add("button", () => {
     const button = document.createElement("button");
     button.type = "button";
+    button.style = "color:blue;";
     button.innerText = "Hello Buttonz";
     button.addEventListener("click", e => console.log(e));
     return button;
