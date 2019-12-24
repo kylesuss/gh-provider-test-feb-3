cdcf7ab0f127d55bef36067d9b373a6790aed309
Kyle Suss <susskyle@gmail.com>
2019-12-24 09:47:11 -0700

    Do it

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,3 @@
 import { configure } from "@storybook/html";
-import "storybook-chromatic";
 
-// automatically import all files ending in *.stories.js
-const req = require.context("../stories", true, /\.stories\.js$/);
-function loadStories() {
-  req.keys().forEach(filename => req(filename));
-}
-
-configure(loadStories, module);
+configure(require.context("../stories", true, /\.stories\.js$/), module);
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,18 +1,54 @@
 import { document, console } from "global";
-import { storiesOf } from "@storybook/html";
 
-storiesOf("Demo", module)
-  .add("heading level 1", () => "<h1>Hello World</h1>")
-  .add("heading level 2", () => "<h2>Hello World</h2>")
-  .add("heading level 3", () => "<h3>Hello World</h3>")
-  .add("heading level 4", () => "<h4>Hello World</h4>")
-  .add("heading level 5", () => "<h5>Hello World</h5>")
-  .add("paragraph", () => "The quick brown fox jumped over the lazy dog.")
-  .add("button", () => {
-    const button = document.createElement("button");
-    button.type = "button";
-    button.style = "color:blue;";
-    button.innerText = "Hello Buttonz";
-    button.addEventListener("click", e => console.log(e));
-    return button;
-  });
+export default {
+  title: "Demo"
+};
+
+export const HeadingLevel1 = () => "<h1>Heading Level 1</h1>";
+
+HeadingLevel1.story = {
+  name: "heading level 1"
+};
+
+export const HeadingLevel2 = () => "<h2>Heading Level 2</h2>";
+
+HeadingLevel2.story = {
+  name: "heading level 2"
+};
+
+export const HeadingLevel3 = () => "<h3>Heading Level 3</h3>";
+
+HeadingLevel3.story = {
+  name: "heading level 3"
+};
+
+export const HeadingLevel4 = () => "<h4>Heading Level 4</h4>";
+
+HeadingLevel4.story = {
+  name: "heading level 4"
+};
+
+export const HeadingLevel5 = () => "<h5>Heading Level 5</h5>";
+
+HeadingLevel5.story = {
+  name: "heading level 5"
+};
+
+export const Paragraph = () => "The quick brown fox jumped over the lazy dog.";
+
+Paragraph.story = {
+  name: "paragraph"
+};
+
+export const Button = () => {
+  const button = document.createElement("button");
+  button.type = "button";
+  button.style = "color:green;";
+  button.innerText = "Hello Button";
+  button.addEventListener("click", e => console.log(e));
+  return button;
+};
+
+Button.story = {
+  name: "button"
+};
