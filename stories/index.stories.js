b6825e97c7e599dd767c9237aefc062eb5f429cd
Kyle Suss <susskyle@gmail.com>
2020-02-03 09:44:37 -0700

    Add new messaging

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,31 +4,32 @@ export default {
   title: "Demo"
 };
 
-export const HeadingLevel1 = () => "<h1>Heading Level 1</h1>";
+export const HeadingLevel1 = () => "<h1>Heading Level 1 New</h1>";
 
 HeadingLevel1.story = {
   name: "heading level 1"
 };
 
-export const HeadingLevel3 = () => "<h3>Heading Level 3</h3>";
+export const HeadingLevel3 = () => "<h3>Heading Level 3 New</h3>";
 
 HeadingLevel3.story = {
   name: "heading level 3"
 };
 
-export const HeadingLevel4 = () => "<h4>Heading Level 4</h4>";
+export const HeadingLevel4 = () => "<h4>Heading Level 4 New</h4>";
 
 HeadingLevel4.story = {
   name: "heading level 4"
 };
 
-export const HeadingLevel5 = () => "<h5>Heading Level 5</h5>";
+export const HeadingLevel5 = () => "<h5>Heading Level 5 New</h5>";
 
 HeadingLevel5.story = {
   name: "heading level 5"
 };
 
-export const Paragraph = () => "The quick brown fox jumped over the lazy dog.";
+export const Paragraph = () =>
+  "The quick brown fox jumped over the lazy dog. New";
 
 Paragraph.story = {
   name: "paragraph"
@@ -38,7 +39,7 @@ export const Button = () => {
   const button = document.createElement("button");
   button.type = "button";
   button.style = "color:green;";
-  button.innerText = "Hello Button";
+  button.innerText = "Hello Button New";
   button.addEventListener("click", e => console.log(e));
   return button;
 };
